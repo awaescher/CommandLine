fcbdf05802561c7d19b51bbe6d10acd07a1dc6ad
Andreas Wäscher <awaescher@sodacore.net>
2018-10-09 12:24:03 +0200

    Show length of arguments

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -8,6 +8,84 @@ namespace CommandLine
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("CommandLine is:");
+
+            Console.ResetColor();
+
+			if (args == null || args.Length == 0)
+			{
+				Console.WriteLine("(EMPTY)");
+			}
+			else
+			{
+				Console.WriteLine(string.Join(" ", args));
+				Console.WriteLine(string.Join(" ", GetLengthStrings(args)));
+			}
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("");
+            Console.WriteLine("Press space to enter an exit code or any other key to quit.");
+            if (Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                int? exitCode = null;
+
+                while (exitCode == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine();
+                    Console.Write("Enter a numeric exit code: ");
+                    string number = Console.ReadLine();
+
+                    int parsed = -1;
+                    if (Int32.TryParse(number, out parsed))
+                    {
+                        exitCode = parsed;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Could not parse \"" + number + "\" to a numeric exit code.");
+                    }
+                }
+
+                Environment.Exit(exitCode.GetValueOrDefault());
+            }
         }
-    }
+
+		private static string[] GetLengthStrings(string[] args)
+		{
+			var result = new string[args.Length];
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				var length = args[i].Length;
+
+
+				if (length == 0)
+				{
+					result[i] = "";
+				}
+				else
+				{
+					result[i] = "".PadRight(length, '─');
+
+					if (length == 1)
+						result[i] = "^";
+					else
+						result[i] = "└" + result[i].Substring(1, length - 2) + "┘";
+
+					if (length > 3)
+					{
+						var lenghtStringLength = length.ToString().Length;
+						int charsLeftRight = (result[i].Length - lenghtStringLength) / 2;
+						int charsLeft = (charsLeftRight * 2 + lenghtStringLength == result[i].Length) ? charsLeftRight : charsLeftRight + 1;
+						result[i] = result[i].Substring(0, charsLeft) + length.ToString() + result[i].Substring(result[i].Length - charsLeftRight, charsLeftRight);
+					}
+				}
+			}
+
+			return result;
+		}
+	}
 }
---
+++ ExitCode/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ExitCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            if (args?.Length != 1)
+            {
+                Console.WriteLine($"Drag an executable on {typeof(Program).Assembly.GetName().Name}.exe to deterine its exit code.");
+                Console.ReadKey();
+                return;
+            }
+
+            string file = args[0];
+            if (!File.Exists(file))
+            {
+                Console.WriteLine("The provided file does not exist: " + file);
+                Console.ReadKey();
+                return;
+            }
+
+            var pi = new ProcessStartInfo(file);
+            pi.RedirectStandardError = true;
+            pi.RedirectStandardOutput = true;
+            pi.UseShellExecute = false;
+
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Starting process: " + Path.GetFileName(file));
+
+                using (var proc = Process.Start(pi))
+                {
+                    proc.ErrorDataReceived += Proc_ErrorDataReceived;
+                    proc.OutputDataReceived += Proc_OutputDataReceived;
+
+                    proc.BeginErrorReadLine();
+                    proc.BeginOutputReadLine();
+
+
+                    proc.WaitForExit();
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Exit code was: " + proc.ExitCode);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("An error occured:\n\n" + ex.Message);
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
+
+        private static void Proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(">> " + e.Data);
+        }
+
+        private static void Proc_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(">> " + e.Data);
+        }
+    }
+}
